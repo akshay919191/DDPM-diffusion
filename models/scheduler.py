2b8724c6a7e621e0519de4f3f0000d20bb266062
akshayyyy <sharmaakshay86117@gmail.com>
2026-03-05 20:57:43 +0530

    updated
    Updated scheduler function to accept dynamic timesteps and added noise addition and step functions.

--- models/scheduler.py
+++ models/scheduler.py
@@ -1,11 +1,12 @@
-import torch , math 
-import torch.nn as nn 
 
+import torch , math
+import torch.nn as nn
+import torch.nn.functional as F
 
 ##
 # scheduler
 def scheduler(timesteps = 1000):
-    beta = torch.linspace(1e-4 , 0.02 , 1000)
+    beta = torch.linspace(1e-4 , 0.02 , timesteps , device = 'cuda')
     alpha = 1 - beta
     alpha_hat = torch.cumprod(alpha , dim = 0)
 
@@ -22,3 +23,35 @@ def scheduler(timesteps = 1000):
 
 
 
+## 
+
+sched = scheduler()
+
+def nosieADD(x0, noise, t):
+
+    device = x0.device   
+
+    s1 = sched['alphahataqrt'].to(device)[t].view(-1,1,1,1)
+    s2 = sched['oneminussqrthat'].to(device)[t].view(-1,1,1,1)
+
+    xt = s1 * x0 + s2 * noise
+
+    return xt, noise
+
+
+def step(x_t, predicted_noise, t):
+
+    beta_t = sched['beta'][t].view(-1, 1, 1, 1)
+    alpha_t = sched['alpha'][t].view(-1, 1, 1, 1)
+    alpha_bar_t = sched['alpha_hat'][t].view(-1, 1, 1, 1)
+    
+    coeff = (1 - alpha_t) / torch.sqrt(1 - alpha_bar_t)
+    mean = (1 / torch.sqrt(alpha_t)) * (x_t - coeff * predicted_noise)
+    
+    if t[0] > 0:
+        noise = torch.randn_like(x_t)
+        
+        sigma_t = torch.sqrt(beta_t) 
+        return mean + sigma_t * noise
+    else:
+        return mean 
